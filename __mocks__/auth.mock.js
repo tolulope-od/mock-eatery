49bffc0b6e905b57ae2ca62086a69abbe6a1cb0b
Tolulope Odueke <tolulope.od@gmail.com>
2019-10-24 15:39:48 +0100

    chore(test):add auth tests

---
+++ __mocks__/auth.mock.js
@@ -0,0 +1,34 @@
+export const validUserSignUp = {
+  firstName: 'Mitt',
+  lastName: 'Romney',
+  email: 'mitt@romney.com',
+  password: 'SomeCoolPassword1'
+};
+
+export const emptyFirstNameSignUp = {
+  firstName: '',
+  lastName: 'Wayne',
+  email: 'bruce@wayne.com',
+  password: 'SomeCoolPassword1'
+};
+
+export const emptyLastNameSignUp = {
+  firstName: 'Bruce',
+  lastName: '',
+  email: 'mitt@romney.com',
+  password: 'SomeCoolPassword1'
+};
+
+export const wronglyFormattedPassword = {
+  firstName: 'Bruce',
+  lastName: 'Wayne',
+  email: 'bruc@batman.com',
+  password: 'authorfleck'
+};
+
+export const emptyEmailSignUp = {
+  firstName: 'Bruce',
+  lastName: 'Wayne',
+  email: '',
+  password: 'SomeCoolPassword1'
+};
